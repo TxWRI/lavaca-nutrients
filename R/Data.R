b6172ee210b3af26c716e735e6ae0250e8182dbe
Michael Schramm <michael.schramm@agnet.tamu.edu>
2022-10-05 16:52:10 -0500

    twdb data is in ac-feet per day

--- R/Data.R
+++ R/Data.R
@@ -54,11 +54,14 @@ download_Q_data <- function(usgs_dir = "data/USGS",
   ## read TWDB data
   
   # texana is gaged flow and can be used as is.
+  # need to convert from acre-feet per day to cubic feet per second!
   texana <- read_table("data/TWDB/Gaged/lktexanag", col_types = "nnnn_") |> 
     pivot_longer(cols = lktexana_g, names_to = "site_no", values_to = "Flow") |> 
     mutate(Date = lubridate::ymd(paste0(year,"-", month,"-", day)),
            agency_cd = "TWDB") |> 
-    select(-c(year, month, day))
+    select(-c(year, month, day)) |> 
+    #convert to cubic feet per second
+    mutate(Flow = (Flow * (43560/86400)))
   
   q |> 
     bind_rows(texana)
@@ -220,7 +223,7 @@ clean_data <- function(wq_df, q_df) {
 fn_data <-   function(model_data) {
   
   grouped <- model_data |> 
-    dplyr::select(site_no, log1p_Flow, stfa, ma, yday) |> 
+    dplyr::select(site_no, Flow, log1p_Flow, stfa, ma, yday) |> 
     group_by(site_no, yday) #|> 
   
   
@@ -231,3 +234,32 @@ fn_data <-   function(model_data) {
   
 }
 
+
+fn_lk_data <-   function(model_data) {
+  
+  inflow <- model_data |>
+    filter(site_no != "usgs08164000") |> 
+    select(c(Date, site_no, Flow, yday)) |> 
+    pivot_wider(names_from = site_no,
+                values_from = Flow) |> 
+    mutate(inflow = usgs08164390 + usgs08164450 + usgs08164503 + usgs08164504) |> 
+    select(Date, Flow = lktexana_g, inflow, yday) |> 
+    mutate(
+      log1p_inflow = log1p(inflow),
+      # flow anomalies
+      ltfa = fa(inflow+1, Date, T_1 = "1 year",
+                T_2 = "period", transform = "log"),
+      stfa = fa(inflow+1, Date, T_1 = "1 day",
+                T_2 = "1 month", transform = "log"),
+      # smooth discounted flow
+      ma = sdf(log1p(inflow))) |>
+    dplyr::select(Flow, inflow, log1p_inflow, stfa, ma, yday) |>
+    group_by(yday)
+  
+  
+  model_data |>
+    filter(site_no == "lktexana_g") |>
+    ungroup()  |> 
+    dplyr::select(site_no, yday, ddate, Date) |> 
+    left_join(inflow)
+}
