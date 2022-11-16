4b14d130d68a1b37d93c0da97748ec0b7da1f248
Michael Schramm <michael.schramm@agnet.tamu.edu>
2022-11-16 12:47:14 -0600

    metadata dataspice script

--- R/metadata.R
+++ R/metadata.R
@@ -8,10 +8,19 @@ prep_access(data_path = "data/Output",
             access_path = "data/Output/metadata/access.csv",
             recursive = TRUE)
 
+edit_access(metadata_dir = "data/Output/metadata")
 
 prep_attributes(data_path = "data/Output",
                 attributes_path = "data/Output/metadata/attributes.csv",
                 recursive = TRUE)
 
-
 edit_attributes(metadata_dir = "data/Output/metadata")
+
+edit_creators(metadata_dir = "data/Output/metadata")
+
+edit_biblio(metadata_dir = "data/Output/metadata")
+
+write_spice(path = "data/Output/metadata")
+
+
+build_site("data/Output/metadata/dataspice.json")
